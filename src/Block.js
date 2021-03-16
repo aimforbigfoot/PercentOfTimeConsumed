6923926e136637637dba9d914474f98019c1b6c2
aimforbigfoot <61890796+aimforbigfoot@users.noreply.github.com>
2021-03-16 14:00:55 -0400

    fixed issue with bar

--- src/Block.js
+++ src/Block.js
@@ -2,7 +2,7 @@ const Block = (props) => {
   return ( 
     <div className="flex flex-wrap justify-around mw9 w-80 ml-auto mr-auto items-center">
       <p>Percent Of {props.message} Consumed: {props.number.toFixed(props.toFixedNum)}</p>
-      <progress className=" br-pill w-30 ml-auto-l " max="100" value={props.number}></progress>
+      <progress className=" br-pill w-30 w-100-s ml-auto-l " max="100" value={props.number}></progress>
     </div>
    );
 }
